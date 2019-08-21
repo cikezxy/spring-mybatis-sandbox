9f8b773ac9d4e7febb57e6647d77ea4dd2ae8549
James.Zheng <James.Zheng@homecredit.cn>
2019-08-21 13:15:32 +0800

    commit

---
+++ src/main/java/com/cikezxy/sandbox/spring/mybatis/MybatisSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.cikezxy.sandbox.spring.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisSandboxApplication.class, args);
+    }
+
+}
